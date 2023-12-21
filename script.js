566f14910cffacd34475e7595c0b0a7c4a9c20c4
DJ <119585273+Chetanthakur03@users.noreply.github.com>
2023-12-21 18:22:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Add your form submission logic here, e.g., send data to a server or display a thank you message.
+    alert("Form submitted!");
+});
